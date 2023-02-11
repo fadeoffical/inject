722f710a17b30c6a8cb8aa4aafcc85dd8ba088d7
fade <truefadeoffical@gmail.com>
2023-02-11 11:48:14 +0100

    Migrate to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    api("org.jetbrains:annotations:24.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
+group = "fade"
+version = "1.0.0"
+description = "inject"
+java.sourceCompatibility = JavaVersion.VERSION_19
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
